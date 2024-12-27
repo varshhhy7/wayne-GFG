cc807b4dfb39fccd08bc9ffe7681181bd78b167b
Varshith <manivarshithpc@gmail.com>
2024-12-27 10:21:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Hard/Median of 2 Sorted Arrays of Different Sizes/median-of-2-sorted-arrays-of-different-sizes.cpp
@@ -0,0 +1,94 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    double medianOf2(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size();
+        int n = nums2.size();
+        
+        vector<int> temp(m+n);
+        
+        int i=0;//nums1
+        int j=0;//nums2
+        
+        int k=0;//temp
+        
+        while(i<m && j<n){
+            if(nums1[i]<nums2[j]){
+                temp[k++]=nums1[i++];
+            }
+            else{
+                temp[k++]=nums2[j++];
+            }
+        }
+        while(i<m){
+            temp[k++]=nums1[i++];
+        }
+        while(j<n){
+            temp[k++]=nums2[j++];
+        }
+        int size=m+n;
+        if(size%2==1){
+            //odd
+            return temp[size/2];
+        }
+        return (temp[size/2]+temp[(size/2)-1])/2.0;
+        
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore(); // Ignore the newline after reading `t`
+
+    while (t--) {
+        // Read the first array
+        string line1;
+        getline(cin, line1);
+        stringstream ss1(line1);
+
+        vector<int> array1;
+        int num;
+        while (ss1 >> num) {
+            array1.push_back(num);
+        }
+
+        // Read the second array
+        string line2;
+        getline(cin, line2);
+        stringstream ss2(line2);
+
+        vector<int> array2;
+        while (ss2 >> num) {
+            array2.push_back(num);
+        }
+
+        // Create a Solution object and calculate the median
+        Solution ob;
+        if (array2.size() == 1 && array2[0] == 0) {
+            array2.pop_back();
+        }
+
+        double ans = ob.medianOf2(array1, array2);
+        if (ans == (int)ans) {
+            cout << (int)ans << endl;
+        } else {
+            cout << ans << endl;
+        }
+
+        cout << "~" << endl;
+    }
+
+    return 0;
+}
+
+// } Driver Code Ends
